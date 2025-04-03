b6b33cc3057fc08b28d3b39c7a0b037fd2a75b18
Tanzeem Hasan <thasan50@stuy.edu>
2025-04-03 10:59:44 -0400

    end of class

--- app/db.py
+++ app/db.py
@@ -8,7 +8,26 @@ def setup():
     db = sqlite3.connect(DB_FILE, check_same_thread=False)
     c = db.cursor()
     c.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password_hash TEXT);")
-    c.execute("CREATE TABLE IF NOT EXISTS airQuality (id INTEGER PRIMARY KEY, pollutant TEXT: 'O3', 'CO', 'SO2', 'NO2', value REAL, state TEXT, year INTEGER, userID INTEGER);")
-    c.execute("CREATE TABLE IF NOT EXISTS congestion (id INTEGER PRIMARY KEY, congestion_index REAL, state TEXT, userID INTEGER, year INTEGER);")
+    c.execute("CREATE TABLE IF NOT EXISTS airQuality (id INTEGER PRIMARY KEY, pollutant TEXT: 'O3', 'CO', 'SO2', 'NO2', value REAL, state TEXT, year INTEGER);")
+    c.execute("CREATE TABLE IF NOT EXISTS congestion (id INTEGER PRIMARY KEY, congestion_index REAL, state TEXT, year INTEGER);")
+    c.execute("CREATE TABLE IF NOT EXISTS congestion_long_term(id INTEGER PRIMARY KEY, congestion_index INTEGER, state TEXT);")
+
+    db.commit()
+    db.close()
+
+def addCongestionOverall():
+    db = sqlite3.connect(DB_FILE, check_same_thread=False)
+    c = db.cursor()
+    with open('congestion.csv', 'r') as congestion:
+        csvValue = csv.DictReader(congestion)
+        for row in csvValue:
+             db.execute("INSERT INTO congestion_long_term(id, congestion_index, state) VALUES (?, ?, ?, ?)", (id, csvValue[28], csvValue[0]))
+    db.commit()
+    db.close()
+
+def getCongestionOverall():
+    db = sqlite3.connect(DB_FILE, check_same_thread=False)
+    c = db.cursor()
+    c.execute("SELECT * FROM " + congestion_long_term + "")
     db.commit()
     db.close()
