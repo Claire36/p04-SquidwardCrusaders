ee8abb527f54bb62896807b60e6d9cb10670a5a4
Linda <lzheng50@stuy.edu>
2025-04-10 10:33:30 -0400

    chloropleht map

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -8,8 +8,8 @@ var pollution = d3.map();
 //given geojson file, it'll help define the path that will be drawn
 var path = d3.geoPath();
 var projection = d3.geoMercator()
-  .scale(70)
-  .center([0,20])
+  .scale(600)
+  .center([-120,50])
   .translate([width/2, height/2]);
 
 var x = d3.scaleLinear().domain([1,10]).rangeRound([600,860]);
@@ -20,9 +20,9 @@ var x = d3.scaleLinear().domain([1,10]).rangeRound([600,860]);
 
 d3.queue()
   .defer(d3.json, "https://gist.githubusercontent.com/sdwfrost/d1c73f91dd9d175998ed166eb216994a/raw/e89c35f308cee7e2e5a784e1d3afc5d449e9e4bb/counties.geojson")
-  await(ready);
+  .await(ready);
 
-function reader(error, topo){
+function ready(error, topo){
   svg.append("g")
     .selectAll("path")
     .data(topo.features)
@@ -31,6 +31,6 @@ function reader(error, topo){
       .attr("d", d3.geoPath()
         .projection(projection)
       )
-      .attr("fill", "lightgrey")
+      .attr("fill", "lightblue")
       .attr("stroke-width", 0.5);
 }
